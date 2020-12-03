0cf33fe01a0d107e17a8d0e7f231f3d0eadfd6d0
Toth Peter <peter.roland.toth@gmail.com>
2020-12-03 07:44:56 +0100

    Simplified solution for day 3

--- Day 3/prog.py
+++ Day 3/prog.py
@@ -1,3 +1,6 @@
+import math
+
+
 def count_trees(forest, step):
     pos = (0, 0)
     nr_trees = 0
@@ -19,8 +22,4 @@ with open("input") as f:
     print("Part 1: ", count_trees(forest, (3, 1)))
 
     to_check = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
-    product = 1
-    for step in to_check:
-        product *= count_trees(forest, step)
-
-    print("Part 2: ", product)
+    print("Part 2: ", math.prod([count_trees(forest, x) for x in to_check]))
